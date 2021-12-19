e509f8dd3d46d09192191ca50f665d885879c062
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-12-19 11:36:33 -0300

    fix: Eslint prefer default export

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 
-import { Title } from './components';
+import Title from './components';
 
 import CssReset from './CssReset';
 
--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,1 +1,1 @@
-export { default as Title } from './Title/Title';
+export { default } from './Title/Title';
