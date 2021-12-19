ea5545a93b2357811047dae464ec085479eb1499
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-12-19 15:32:58 -0300

    Create the page Start
    The StartPage will organize the first page of the App.

--- src/components/index.jsx
+++ src/components/index.jsx
@@ -1,3 +1,4 @@
 export { default as Title } from './Title/Title';
 export { default as Settings } from './Settings/Settings';
 export { default as Button } from './Button/Button';
+export { default as PageWrapper } from './PageWrapper/PageWrapper';
