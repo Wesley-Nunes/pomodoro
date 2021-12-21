3489bae796772fae09f0b9cee352b6283a0285a8
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-12-21 15:58:41 -0300

    feat: Create the router

---
+++ src/services/index.jsx
@@ -0,0 +1,2 @@
+export { BrowserRouter, Link } from 'react-router-dom';
+export { default as Router } from './router/Router';
---
+++ src/services/router/Router.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+import { Route, Routes } from 'react-router-dom';
+
+import StartPage from '../../pages/StartPage';
+import WorkTimePage from '../../pages/WorkTimePage';
+import BreakTimePage from '../../pages/BreakTimePage';
+
+function Router() {
+  return (
+    <Routes>
+      <Route path="/" element={<StartPage />} />
+      <Route path="/work-time" element={<WorkTimePage />} />
+      <Route path="/break-time" element={<BreakTimePage />} />
+    </Routes>
+  );
+}
+
+export default Router;
