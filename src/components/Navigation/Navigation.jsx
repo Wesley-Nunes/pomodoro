49402e85f46e56d42256bf7f5e7175a2189d9ec0
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-12-27 20:27:03 -0300

    style: Improve the Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -1,15 +1,35 @@
 import React from 'react';
 
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 import Button from '../Button/Button';
 
-function Navigation() {
+import NavigationWrapper from './NavigationStyle';
+
+function StartPageNav() {
   return (
-    <Link to="/work-time">
-      <Button bgColor="primary">Start</Button>
-    </Link>
+    <NavigationWrapper>
+      <Link to="/work-time">
+        <Button bgColor="primary">Start</Button>
+      </Link>
+    </NavigationWrapper>
   );
+}
+
+function TimerPageNav() {
+  return (
+    <NavigationWrapper>
+      <Link to="/">
+        <Button bgColor="nullColor">Restart</Button>
+      </Link>
+      <Button bgColor="secondary">Pause</Button>
+    </NavigationWrapper>
+  );
+}
+
+function Navigation() {
+  const { pathname } = useLocation();
+  return pathname === '/' ? <StartPageNav /> : <TimerPageNav />;
 }
 
 export default Navigation;
