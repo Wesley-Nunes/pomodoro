69a3ac65664780602b4d9cb787ae563b3930bf83
Wesley-Nunes <wesley_info@yahoo.com.br>
2021-12-27 18:36:11 -0300

    feat: Implement the getValues in the StartPage

--- src/pages/StartPage.jsx
+++ src/pages/StartPage.jsx
@@ -3,15 +3,19 @@ import React from 'react';
 import {
   Title, Settings, PageWrapper, Button, MyLink,
 } from '../components';
+import { useTimer } from '../services';
 
 function StartPage() {
+  const { getValues } = useTimer();
+  const { sessions, workTime, breakTime } = getValues();
+
   return (
     <PageWrapper>
       <Title />
       <div>
-        <Settings name="Work Time" value="25:00" />
-        <Settings name="Break Time" value="5:00" />
-        <Settings name="Sessions" value="4" />
+        <Settings name="Work Time" value={`${workTime.minutes}:${workTime.seconds}`} />
+        <Settings name="Break Time" value={`${breakTime.minutes}:${breakTime.seconds}`} />
+        <Settings name="Sessions" value={`${sessions}`} />
       </div>
       <MyLink targetPage="/work-time">
         <Button bgColor="primary">Start</Button>
